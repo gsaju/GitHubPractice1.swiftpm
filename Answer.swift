9d712b1ac2da2f26fc804be2da05e8c0ab929f17
Kevin Benny <kbenny5025@stu.d214.oeg>
2023-04-14 13:01:35 -0500

    Answer

---
+++ Answer.swift
@@ -0,0 +1,18 @@
+//
+//  Answer.swift
+//  GitHubPractice1
+//
+//  Created by Kevin Benny on 4/14/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Answer: View {
+    @State var answer:String
+    var body: some View {
+        Button("Multiply"){
+            
+        }
+    }
+}
